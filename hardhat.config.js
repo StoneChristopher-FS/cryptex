1143218075d1e75f25a0649c60b68ebe2d477d12
Christopher Stone <cmstone@student.fullsail.edu>
2023-05-29 00:39:42 -0400

    Deploy contract to test network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+const privateKeys = process.env.PRIVATE_KEYS || ""
+const infuraKey = process.env.INFURA_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
-  netowrks: {
-    localhost: {}
+  networks: {
+    localhost: {},
+    goerli: {
+      url: `https://goerli.infura.io/v3/${infuraKey}`,
+      accounts: privateKeys.split(','),
+    }
   },
 };
